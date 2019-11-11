4449bed202cc7f3348467ea646eaee593bed319b
CHS-19970810 <13106580686@163.com>
2019-11-11 15:59:15 +0800

    1.新增了AddTest.java 2.修改了Demo01.java

---
+++ src/com/chs/Demo/AddTest.java
@@ -0,0 +1,7 @@
+package com.chs.Demo;
+
+public class AddTest {
+    public static void main(String[] args) {
+        System.out.println("增加");
+    }
+}
--- src/com/chs/Demo/Demo01.java
+++ src/com/chs/Demo/Demo01.java
@@ -3,5 +3,6 @@ package com.chs.Demo;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("GitHub");
+        System.out.println("修改");
     }
 }
